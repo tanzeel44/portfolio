8d685f8afd6312f27e7e7d66f58afc246b03dd3d
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-06-03 17:24:00 -0400

    setup testing/eslint

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,9 @@ export default function Home() {
         <meta name="description" content="Tanzeel ur Rehman's Web Development Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main>
+        <h1>Come to Axe!</h1>
+      </main>
     </div>
   );
 }

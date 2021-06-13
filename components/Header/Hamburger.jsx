5ae283efa957acab678ab824ee30b751560d47f7
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-06-12 21:16:35 -0400

    hamburger redone

--- components/Header/Hamburger.jsx
+++ components/Header/Hamburger.jsx
@@ -1,81 +1,102 @@
 import styled from 'styled-components';
 
 const StyledHamburger = styled.div`
-  position: relative;
-  width: 50px;
-  height: 50px;
+  flex: 0 0 auto;
+  width: 40px;
+  height: 40px;
+  border: 1px solid rgb(15,15,15);
   cursor: pointer;
-  transform: scale(1.5);
-  z-index: 10;
+  position: relative;
+  
+  .hamburger,
+  .cross {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+  }
 
-  .menuIconCheckbox {
+  .hamburger span {
     display: block;
+    width: 18px;
+    height: 2px;
+    margin-bottom: 3px;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .hamburger span:last-child {
+    margin: 0;
+  }
+
+  .hamburger span:before,
+  .hamburger span:after {
+    content: "";
+    position: absolute;
     width: 100%;
     height: 100%;
-    position: relative;
-    cursor: pointer;
-    z-index: 2;
-    -webkit-touch-callout: none;
-    position: absolute;
-    opacity: 0;
+    background-color: rgb(15,15,15);
+    transform: translateX(-200%);
+    transition: transform ease 300ms;
   }
 
-  div {
-    margin: auto;
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    width: 22px;
-    height: 12px;
+  .hamburger span:after {
+    transform: translateX(0);
   }
 
-  span {
-    position: absolute;
+  .hamburger span:nth-child(2):before,
+  .hamburger span:nth-child(2):after {
+    transition-delay: 75ms;
+  }
+
+  .hamburger span:last-child:before,
+  .hamburger span:last-child:after {
+    transition-delay:150ms;
+  }
+
+  &.active .hamburger span:before {
+    transform: translateX(100%);
+  }
+
+  &.active .hamburger span:after {
+    transform: translateX(200%);
+  }
+
+  .cross span {
     display: block;
-    width: 100%;
+    width: 18px;
     height: 2px;
-    background-color: #000;
-    border-radius: 1px;
-    transition: all 0.2s cubic-bezier(0.1, 0.82, 0.76, 0.965);
-
-    &:first-of-type {
-      top: 0;
-    }
-
-    &:last-of-type {
-      bottom: 0;
-    }
+    background-color: rgb(15,15,15);
+    transform: translateY(50%) rotate(45deg) scaleX(0);
+    transition: transform ease 200ms;
   }
 
-  &.active,
-  .menuIconCheckbox:checked + div {
-    span {
-      &:first-of-type {
-        transform: rotate(45deg);
-        top: 5px;
-      }
-      &:last-of-type {
-        transform: rotate(-45deg);
-        bottom: 5px;
-      }
-    }
+  .cross span:last-child {
+    transform: translateY(-50%) rotate(-45deg) scaleX(0);
   }
 
-  &.active:hover span:first-of-type,
-  &.active:hover span:last-of-type,
-  &:hover .menuIconCheckbox:checked + div span:first-of-type,
-  &:hover .menuIconCheckbox:checked + div span:last-of-type {
-    width: 22px;
+  &.active .cross span {
+    transition-delay: 450ms;
+    transform: translateY(50%) rotate(45deg) scaleX(1);
+  }
+
+  &.active .cross span:last-child {
+    transform: translateY(-50%) rotate(-45deg) scaleX(1);
   }
 `;
 
 export default function Hamburger({ openHandler, openState }) {
   return (
-    <StyledHamburger>
-      <input onChange={() => openHandler(!openState)} className="menuIconCheckbox" type="checkbox" />
-      <div>
+    <StyledHamburger
+      onClick={() => openHandler(!openState)}
+      className={openState ? 'active' : ''}
+    >
+      <div className="hamburger">
+        <span />
+        <span />
+        <span />
+      </div>
+      <div className="cross">
         <span />
         <span />
       </div>

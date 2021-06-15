28466533ca7349982ae063ff8319f7d1bb4ae36a
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-06-15 17:49:47 -0400

    removed unnecessary import

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 
 import Navbar from './Navbar';

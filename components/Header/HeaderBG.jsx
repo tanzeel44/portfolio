aeefc1d3d9f8464c686c1794dd9342a63492b271
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-06-21 17:25:28 -0400

    header edited for mobile

--- components/Header/HeaderBG.jsx
+++ components/Header/HeaderBG.jsx
@@ -26,16 +26,18 @@ export default function HeaderBG() {
 
     // resize blob responsively with window resize to ensure proper logo/menu positioning
     window.addEventListener('resize', changeViewboxWithViewport);
-    // changeViewboxWithViewport();
+    changeViewboxWithViewport();
   }, []);
 
   return (
     <svg
       style={{
         position: 'absolute',
+        top: 0,
+        left: 0,
       }}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox={`0 0 ${viewBoxWidth} 320`}
+      viewBox={`0 0 ${viewBoxWidth} 290`}
     >
       <path fill="#00cba9" fillOpacity="1" d="M0,128L34.3,117.3C68.6,107,137,85,206,101.3C274.3,117,343,171,411,176C480,181,549,139,617,117.3C685.7,96,754,96,823,106.7C891.4,117,960,139,1029,170.7C1097.1,203,1166,245,1234,229.3C1302.9,213,1371,139,1406,101.3L1440,64L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z" />
     </svg>

7283351a076076d1ab1b0b13bce3c9e25c71bed2
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-07-12 16:18:40 -0400

    replace vh/vw with dvh/dvw

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ import Header from '../Header/Header';
 import Global from '../../styles/Global';
 
 const Page = styled.div`
-  width: 100vw;
+  width: 100dvw;
   min-width: 280px;
   background-color: ${(props) => props.theme.background};
   color: ${(props) => props.theme.color};

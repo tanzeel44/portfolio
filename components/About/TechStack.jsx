c588ab587d80d8711e0c1efcbc1f5bf699e9a2cb
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-07-17 21:29:33 -0400

    grid updated

--- components/About/TechStack.jsx
+++ components/About/TechStack.jsx
@@ -5,10 +5,10 @@ import TechCard from './TechCard';
 
 const Tech = styled(motion.div)`
   height: 100%;
-  border: 10px solid green;
 
   h1 {
     font-size: 3rem;
+    padding-bottom: 1rem;
     text-align: center;
     
     @media only screen and (min-width: 375px) and (max-width: 539px) {
@@ -21,17 +21,26 @@ const Tech = styled(motion.div)`
   }
 
   .tech {
-    height: calc(100% - 3rem);
+    height: calc(100% - 4rem);
     width: 100%;
     padding-top: 10px;
     overflow-y: auto;
+    display: grid;
+    grid-template-columns: 100%;
+    grid-row-gap: 2rem;
 
-    @media only screen and (min-width: 375px) and (max-width: 539px) {
-      height: calc(100% - 4rem);
+    @media only screen and (min-width: 360px) and (max-width: 539px) {
+      height: calc(100% - 5rem);
+      grid-template-columns: repeat(2, 50%);
     }
 
-    @media only screen and (min-width: 540px) {
-      height: calc(100% - 5rem);
+    @media only screen and (min-width: 540px) and (max-width: 991px) {
+      height: calc(100% - 6rem);
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media only screen and (min-width: 992px) { 
+      
     }
   }
 
@@ -40,6 +49,12 @@ const Tech = styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+
+  .learning-tech {
+    font-size: 2rem;
+    text-align: center;
+    grid-column: 1/-1;
   }
 `;
 
@@ -66,6 +81,12 @@ export default function TechStack() {
       <h1>My Tech</h1>
       <section className="tech">
         {techStack.map((tech) => (
+          <div className="card-container" key={tech.name}><TechCard image={tech.image} title={tech.name} /></div>
+        ))}
+        <h2 className="learning-tech">
+          Currently Learning:
+        </h2>
+        {learning.map((tech) => (
           <div className="card-container" key={tech.name}><TechCard image={tech.image} title={tech.name} /></div>
         ))}
       </section>

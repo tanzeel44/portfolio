49bc571bb8807c7641ccecd1f3c76ef506f96a81
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-08-02 10:32:34 -0400

    email provider changed

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,34 +1,18 @@
-const nodemailer = require('nodemailer');
+import sgMail from '@sendgrid/mail';
 
 export default function (req, res) {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      type: 'OAuth2',
-      user: process.env.EMAIL,
-      clientId: process.env.CLIENTID,
-      clientSecret: process.env.CLIENTSECRET,
-      refreshToken: process.env.REFRESHTOKEN,
-      accessToken: process.env.ACCESSTOKEN,
-    },
-  });
-
-  const mailData = {
-    from: process.env.EMAIL,
+  sgMail.setApiKey(process.env.EMAILAPI);
+  const text = `${req.body.name}\n${req.body.email}\n${req.body.message}`;
+  const mail = {
     to: process.env.DESTEMAIL,
-    subject: `Site Inquiry From ${req.body.name}`,
-    text: req.body.message,
-    html: `<div>${req.body.message}</div>`,
+    from: process.env.EMAIL,
+    subject: 'Portfolio Mail',
+    text,
   };
 
-  transporter.sendMail(mailData, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(info);
-    }
+  return new Promise((resolve, reject) => {
+    sgMail.send(mail)
+      .then(() => res.status(200).end)
+      .catch(() => res.status(500).end());
   });
-  res.status(200);
 }
